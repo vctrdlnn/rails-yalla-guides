fac0cd2f6edf23f65b43edcc69b2d8611862b619
Victor Delannoy <victor.delannoy@gmail.com>
2016-11-15 09:35:01 +0100

    Implemented new profile after sign-up

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,8 +3,27 @@ class ProfilesController < ApplicationController
   end
 
   def new
+    @profile = Profile.new
+  end
+
+  def create
+    @profile = Profile.new(guide_params)
+    @profile.user_id = current_user.id
+    if @profile.save
+      redirect_to @profile
+    else
+      render :new
+    end
   end
 
   def edit
+    @profile = Profile.find_by user_id: current_user.id
+    @attributes = Profile.attribute_names - %w(id user_id created_at updated_at)
+  end
+
+  private
+
+  def guide_params
+    params.require(:profile).permit(:role, :user_id)
   end
 end
