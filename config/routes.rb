dd493b491a0959f3b0e3412297df4fbd2847308d
Guilaine Ghossoub <guilaine.ghossoub4@gmail.com>
2016-11-15 11:24:52 +0100

    FACEBOOK CONNECT

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
 
   resources :profiles
 
-  devise_for :users, controllers: { registrations: "registrations" }
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
