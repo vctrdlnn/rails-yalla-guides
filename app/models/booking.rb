1e614cefc36e692ac7e8ff837a852a35c57bda08
Victor Delannoy <victor.delannoy@gmail.com>
2016-11-15 14:20:33 +0100

    First version of booking with new, edit, show, index. seed to add statuses

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,9 @@ class Booking < ApplicationRecord
   belongs_to :guide
   belongs_to :profile
   belongs_to :status
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :guide, presence: true
+  validates :profile, presence: true
 end
